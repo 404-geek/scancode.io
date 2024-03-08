248bb1e91bdeb580bf7b64fb5cd16f89639e5615
Ayan Sinha Mahapatra <ayansmahapatra@gmail.com>
2024-03-08 16:12:28 +0530

    Support fast package-only scan in inspect_packages
    Support the new only_packages attributes in scancode
    get_package_data API, to only scan for package data and
    skip license and copyright detection.
    
    Signed-off-by: Ayan Sinha Mahapatra <ayansmahapatra@gmail.com>

--- scanpipe/pipelines/inspect_packages.py
+++ scanpipe/pipelines/inspect_packages.py
@@ -60,5 +60,9 @@ class InspectPackages(ScanCodebase):
         # only detect package_data in resources and create
         # Package/Dependency instances directly instead of assembling
         # the packages and assigning files to them
-        scancode.scan_for_application_packages(self.project, assemble=False)
+        scancode.scan_for_application_packages(
+            project=self.project,
+            assemble=False,
+            package_only=True,
+        )
         scancode.process_package_data(self.project)
